0b275da47a44e46b14e9cac258c6b170c2b94784
venerealulcer <chenchenbbk@hotmail.com>
2017-12-13 14:00:47 +0800

    Add unit test

---
+++ XMLReflection_UnitTest/UnitTest.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XmlReflection;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XMLReflection_UnitTest
+{
+
+    [TestClass()]
+    public class UnitTest
+    {
+
+
+        [TestMethod()]
+        public void Test_BaseType()
+        {
+            XML_BaseType mXML_Base = new XML_BaseType();
+
+            mXML_Base.Age = 101;
+            mXML_Base.character = 'g';
+            mXML_Base.id = 102;
+            mXML_Base.height = 103;
+            mXML_Base.name = "mytest";
+            mXML_Base.size = 104L;
+            mXML_Base.width = 105.1d;
+            mXML_Base.depth = 106.2M;
+            mXML_Base.flag = false;
+            mXML_Base.date = DateTime.Now;
+
+            var map1 = new Dictionary<decimal, string>();
+            var map2 = new Dictionary<decimal, string>();
+
+            map1.Add(3.2M, "3.2M");
+            map1.Add(4.99992M, "4.99992M");
+            map2.Add(100.01M, "100.01M");
+
+            mXML_Base.dictionary.Add(1, map1);
+            mXML_Base.dictionary.Add(100, map2);
+
+
+            var filename = "xml_basetype.xml";
+            mXML_Base.SaveXML(filename);
+
+            XML_BaseType mXML_Base_copy = new XML_BaseType();
+            mXML_Base_copy.LoadXML(filename);
+
+
+            Assert.AreEqual(mXML_Base.Age, mXML_Base_copy.Age);
+            Assert.AreEqual(mXML_Base.character, mXML_Base_copy.character);
+            Assert.AreEqual(mXML_Base.id, mXML_Base_copy.id);
+            Assert.AreEqual(mXML_Base.height, mXML_Base_copy.height);
+            Assert.AreEqual(mXML_Base.name, mXML_Base_copy.name);
+            Assert.AreEqual(mXML_Base.size, mXML_Base_copy.size);
+            Assert.AreEqual(mXML_Base.width, mXML_Base_copy.width);
+            Assert.AreEqual(mXML_Base.depth, mXML_Base_copy.depth);
+            Assert.AreEqual(mXML_Base.flag, mXML_Base_copy.flag);
+            Assert.AreEqual(mXML_Base.date, mXML_Base_copy.date);
+
+            foreach (var item in mXML_Base.dictionary.Keys)
+            {
+                var value1 = mXML_Base.dictionary[item];
+                var value2 = mXML_Base_copy.dictionary[item];
+
+                foreach (var item1 in value1.Keys)
+                {
+                    var value3 = value1[item1];
+                    var value4 = value1[item1];
+                    Assert.AreEqual(value3, value4);
+                }
+            }
+
+
+        }
+
+
+        [TestMethod()]
+        public void Test_Array()
+        {
+            XML_Array mXML_Array = new XML_Array();
+
+            mXML_Array.a_bool = new[] { true, false, true };
+            mXML_Array.a_datatime = new[] { DateTime.MinValue, DateTime.Now, DateTime.MaxValue };
+            mXML_Array.a_int = new[] { 0, int.MaxValue, int.MinValue };
+            mXML_Array.a_str = new[] { "", "12132435434242354546", "@##$%^^&**(((/*-+-*+_" };
+            mXML_Array.list = new List<int>() { 10, 20, 30, 40 };
+            mXML_Array.a_lisyint[0] = new List<int>() { 1, 3, 4 };
+            mXML_Array.a_lisyint[1] = new List<int>() { 44, 55, 66 };
+            mXML_Array.list_array.Add(new[] { 99, 88, 77 });
+            mXML_Array.list_array.Add(new[] { 199, 188, 177 });
+            mXML_Array.a_a_string[0] = new string[] { "a", "1" };
+            mXML_Array.a_a_string[1] = new string[] { "2", "b" };
+            mXML_Array.a_a_string[2] = new string[] { "3", "c" };
+
+
+            var filename = "xml_array.xml";
+            mXML_Array.SaveXML(filename);
+
+
+            XML_Array mXML_Array_copy = new XML_Array();
+            mXML_Array_copy.LoadXML(filename);
+
+
+            for (int i = 0; i < mXML_Array.a_bool.Length; i++)
+                Assert.AreEqual(mXML_Array.a_bool[i], mXML_Array_copy.a_bool[i]);
+
+            for (int i = 0; i < mXML_Array.a_datatime.Length; i++)
+                Assert.AreEqual(mXML_Array.a_datatime[i], mXML_Array_copy.a_datatime[i]);
+
+            for (int i = 0; i < mXML_Array.a_int.Length; i++)
+                Assert.AreEqual(mXML_Array.a_int[i], mXML_Array_copy.a_int[i]);
+
+            for (int i = 0; i < mXML_Array.a_str.Length; i++)
+                Assert.AreEqual(mXML_Array.a_str[i], mXML_Array_copy.a_str[i]);
+
+            Assert.AreEqual(mXML_Array.a_lisyint.Length, mXML_Array_copy.a_lisyint.Length);
+
+            Assert.AreEqual(mXML_Array.a_lisyint[1][2], mXML_Array_copy.a_lisyint[1][2]);
+
+            for (int i = 0; i < mXML_Array.list_array.Count; i++)
+                for (int j = 0; j < mXML_Array.list_array[i].Length; j++)
+                    Assert.AreEqual(mXML_Array.list_array[i][j], mXML_Array_copy.list_array[i][j]);
+
+            for (int i = 0; i < mXML_Array.a_a_string.Length; i++)
+                for (int j = 0; j < mXML_Array.a_a_string[i].Length; j++)
+                    Assert.AreEqual(mXML_Array.a_a_string[i][j], mXML_Array_copy.a_a_string[i][j]);
+
+
+
+        }
+
+
+        [TestMethod()]
+        public void Test_Bodied()
+        {
+            XML_Bodied mXML_Bodied = new XML_Bodied();
+            mXML_Bodied.a_int = new int[] { int.MinValue, 0, int.MaxValue };
+            mXML_Bodied.Flag = false;
+            mXML_Bodied.list = new List<string>() { "", "1213", "abc", "测试" };
+            mXML_Bodied.Name = "Test_Bodied";
+            mXML_Bodied.Num = 110;
+
+
+            var filename = "xml_Bodied.xml";
+            mXML_Bodied.SaveXML(filename);
+
+            XML_Bodied mXML_Bodied_copy = new XML_Bodied();
+            mXML_Bodied_copy.LoadXML(filename);
+
+            for (int i = 0; i < mXML_Bodied.a_int.Length; i++)
+                Assert.AreEqual(mXML_Bodied.a_int[i], mXML_Bodied_copy.a_int[i]);
+            for (int i = 0; i < mXML_Bodied.list.Count; i++)
+                Assert.AreEqual(mXML_Bodied.list[i], mXML_Bodied_copy.list[i]);
+
+            Assert.AreEqual(mXML_Bodied.Name, mXML_Bodied_copy.Name);
+            Assert.AreEqual(mXML_Bodied.Num, mXML_Bodied_copy.Num);
+            Assert.AreEqual(mXML_Bodied.Flag, mXML_Bodied_copy.Flag);
+
+        }
+
+
+    }
+
+    public class XML_BaseType : XMLBase
+    {
+        //string
+
+        public string name = string.Empty;
+
+        //基元类型
+
+        public bool flag = true;
+
+        private byte age = 0;
+
+        public int id = 0;
+
+        public char character = '0';
+
+        public long size = 0L;
+
+        public float height = 0f;
+
+        public double width = 0d;
+
+        public decimal depth = 0m;
+
+        public Dictionary<int, Dictionary<decimal, string>> dictionary = new Dictionary<int, Dictionary<decimal, string>>();
+
+        //结构和类
+
+        public DateTime date = DateTime.Now;
+
+        public byte Age { get => age; set => age = value; }
+    }
+
+    public class XML_Array : XMLBase
+    {
+        public string[][] a_a_string = new string[3][];
+
+        public List<int[]> list_array = new List<int[]>();
+
+        public List<int> list = new List<int>();
+
+        public List<int>[] a_lisyint = new List<int>[2];
+
+        public int[] a_int = new int[3];
+
+        public string[] a_str = new string[3];
+
+        public bool[] a_bool = new bool[3];
+
+        public DateTime[] a_datatime = new DateTime[3];
+    }
+
+    public class XML_Bodied : XMLBase
+    {
+        public bool Flag { set; get; } = true;
+        public int Num { set; get; } = 100;
+        public string Name { set; get; } = "lucy";
+        public int[] a_int { set; get; } = { 1, 3, 4 };
+        public List<string> list { set; get; } = new List<string>() { "xd", "ff" };
+
+    }
+
+
+
+}
--- XmlReflection/XMLBase.cs
+++ XmlReflection/XMLBase.cs
@@ -1,33 +1,26 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace XMLReflection
+namespace XmlReflection
 {
-
     public class XMLBase : INotifyPropertyChanged
     {
 
         public static readonly Assembly mCoreLib = Assembly.Load("mscorlib");
         public static readonly Assembly mCurrLib = Assembly.GetExecutingAssembly();
 
-        const string IENUMERABLE = "IList";
+        const string IENUMERABLE = "ICollection";
         const BindingFlags NonPublicInstance = BindingFlags.NonPublic | BindingFlags.Instance;
         const BindingFlags PublicInstance = BindingFlags.Public | BindingFlags.Instance;
 
-        public enum SupportEnum
-        {
-            STRING, PRIMARY, CLASS, STRUCT, ENUMERABLE, OTHER
-        }
+        private PropertyInfo[] mProperties = null;
 
         public event PropertyChangedEventHandler PropertyChanged;
-
-        private PropertyInfo[] mProperties = null;
 
         private Dictionary<int, Action> mDictionary = new Dictionary<int, Action>();
 
@@ -36,6 +29,11 @@ namespace XMLReflection
             mProperties = GetType().GetProperties(PublicInstance);
 
             if (mProperties.Length > 0) _NotifyDataSetChanged(this);
+        }
+
+        public enum SupportEnum
+        {
+            STRING, PRIMARY, CLASS, STRUCT, ENUMERABLE, ARRAY, OTHER
         }
 
         private static SupportEnum GetTypeFlag(Type t)
@@ -56,6 +54,10 @@ namespace XMLReflection
             {
                 return SupportEnum.STRUCT;
             }
+            else if (t.IsArray)
+            {
+                return SupportEnum.ARRAY;
+            }
             else if (t.GetInterface(IENUMERABLE) != null)
             {
                 return SupportEnum.ENUMERABLE;
@@ -66,18 +68,39 @@ namespace XMLReflection
             }
         }
 
-
-        string HandleName(object _object)
+        private string HandleName(object _object)
         {
+            if (_object is FieldInfo)
+            {
+                var field = _object as FieldInfo;
+
+                var name = field.Name;
+
+                if (name.Contains("__BackingField"))
+                {
+                    return name.Split('<')[1].Split('>')[0];
+                }
+                else if (name.Contains("<") || name.Contains(">"))
+                {
+                    return name.Replace("<", "_").Replace(">", "_");
+                }
+                else
+                {
+                    return name;
+                }
+            }
+
             var type = _object.GetType();
 
             var orgin_name = _object.GetType().Name;
 
+            if (orgin_name == "KeyValuePair`2") return "keyValue";
+
             var result_name = orgin_name;
 
-            if (_object is System.Collections.IList)
+            if (_object is System.Collections.ICollection)
             {
-                var count = (_object as System.Collections.IList).Count;
+                var count = (_object as System.Collections.ICollection).Count;
 
                 if (orgin_name.Contains("`"))
                 {
@@ -94,12 +117,11 @@ namespace XMLReflection
 
         protected XElement GetXMLIEnumerable(object _object)
         {
-
             XElement xml = new XElement(HandleName(_object));
 
             var type = GetTypeFlag(_object.GetType());
 
-            if (type == SupportEnum.ENUMERABLE)
+            if (type == SupportEnum.ENUMERABLE || type == SupportEnum.ARRAY)
             {
                 var list = _object as System.Collections.IEnumerable;
 
@@ -111,7 +133,8 @@ namespace XMLReflection
             else
             {
                 xml = GetXML(_object);
-                xml.Name = _object.GetType().Name;
+
+                xml.Name = HandleName(_object);
             }
 
             return xml;
@@ -146,63 +169,71 @@ namespace XMLReflection
             }
             else if (type == SupportEnum.CLASS || type == SupportEnum.STRUCT || type == SupportEnum.OTHER)
             {
+                if (_object is Type) _object = Activator.CreateInstance(_object as Type);
+
                 var mem = _object.GetType().GetFields(NonPublicInstance | PublicInstance);
 
                 foreach (var item in mem)
                 {
                     var item_value = item.GetValue(_object);
 
-                    item.SetValue(_object, SetXML(item_value, _elem.Element(item.Name)));
+                    if (item_value == null) throw new NotSupportedException("Field must be initialized");
+
+                    item.SetValue(_object, SetXML(item_value, _elem.Element(HandleName(item))));
                 }
             }
-            else if (type == SupportEnum.ENUMERABLE)
+            else if (type == SupportEnum.ARRAY)
             {
-                var list = _object as System.Collections.IList;
+                var thisType = _object as Type ?? _object.GetType();
 
-                Type list_type = _object.GetType().GetElementType();
+                Type ele_type = thisType.GetElementType() ?? thisType.GetGenericArguments()[0];
 
-                var ele_type = GetTypeFlag(list_type);
-
-                list.Clear();
+                var obj = Activator.CreateInstance(thisType, new object[] { _elem.Elements().Count() }) as System.Collections.IList;
 
                 int index = 0;
 
                 foreach (var ele in _elem.Elements())
                 {
-                    object obj = null;
+                    obj[index] = SetXML(ele_type, ele);
 
-                    if (ele_type == SupportEnum.ENUMERABLE)
+                    index++;
+                }
+
+                _object = obj;
+            }
+            else if (type == SupportEnum.ENUMERABLE)
+            {
+                var thisType = _object as Type ?? _object.GetType();
+
+                Type ele_type = thisType.GetElementType() ?? thisType.GetGenericArguments()[0];
+
+                var obj = Activator.CreateInstance(thisType);
+
+                if (obj is System.Collections.IList)
+                {
+                    var ilist = obj as System.Collections.IList;
+
+                    foreach (var ele in _elem.Elements())
                     {
-                        obj = Activator.CreateInstance(list_type);
+                        ilist.Add(SetXML(ele_type, ele));
                     }
-                    else if (ele_type == SupportEnum.STRING)
-                    {
-                        obj = string.Empty;
-                    }
-                    else if (list is Array)
-                    {
-                        var rank = (list as Array).Rank;
+                }
+                else if (obj is System.Collections.IDictionary)
+                {
+                    var typeKEY = thisType.GetGenericArguments()[0];
 
-                        if (rank == 1)
-                        {
-                            obj = Activator.CreateInstance(list_type);
+                    var typeVALUE = thisType.GetGenericArguments()[1];
 
-                            list[index++] = SetXML(obj, ele);
-                        }
-                        else
-                        {
-                            throw new NotImplementedException("Did not implement multi-dimensional array");
-                        }
-                    }
-                    else
+                    var ilist = obj as System.Collections.IDictionary;
+
+                    foreach (var ele in _elem.Elements())
                     {
-                        obj = Activator.CreateInstance(list_type);
-
-                        list.Add(SetXML(obj, ele));
+                        ilist.Add(SetXML(typeKEY, ele.Element("key")), SetXML(typeVALUE, ele.Element("value")));
                     }
                 }
 
-                _object = list;
+                _object = obj;
+
             }
 
             return _object;
@@ -224,7 +255,7 @@ namespace XMLReflection
 
                 foreach (var item in mem)
                 {
-                    XElement xe = new XElement(item.Name);
+                    XElement xe = new XElement(HandleName(item));
 
                     var itemValue = item.GetValue(_object);
 
@@ -240,7 +271,7 @@ namespace XMLReflection
                     {
                         xe = GetXML(itemValue);
                     }
-                    else if (item_type == SupportEnum.ENUMERABLE)
+                    else if (item_type == SupportEnum.ENUMERABLE || item_type == SupportEnum.ARRAY)
                     {
                         xe = GetXMLIEnumerable(itemValue);
                     }
@@ -249,7 +280,7 @@ namespace XMLReflection
                         xe = GetXML(itemValue);
                     }
 
-                    xe.Name = item.Name;
+                    xe.Name = HandleName(item);
 
                     xml.Add(xe);
                 }
@@ -260,7 +291,7 @@ namespace XMLReflection
 
         public void LoadXML(string filename = "")
         {
-            XElement xml = XElement.Load((filename == "" ? GetType().Name : filename) + ".xml");
+            XElement xml = XElement.Load((filename == "" ? (GetType().Name + ".xml") : filename));
 
             SetXML(this, xml);
 
@@ -271,7 +302,7 @@ namespace XMLReflection
         {
             var temp = GetXML(this);
 
-            temp.Save((filename == "" ? GetType().Name : filename) + ".xml");
+            temp.Save((filename == "" ? (GetType().Name + ".xml") : filename));
         }
 
         protected virtual void OnPropertyChanged(object _object, string name)
@@ -281,6 +312,8 @@ namespace XMLReflection
 
         protected virtual void _NotifyDataSetChanged(object _object)
         {
+            if (mProperties == null) return;
+
             foreach (var item in mProperties)
             {
                 if (mDictionary.ContainsKey(item.MetadataToken))
@@ -300,7 +333,7 @@ namespace XMLReflection
                     }
                     else
                     {
-                        mDictionary.Add(item.MetadataToken, (Action)(() =>
+                        mDictionary.Add(item.MetadataToken, (() =>
                         {
                             OnPropertyChanged(_object, item.Name);
                         }));
@@ -315,114 +348,5 @@ namespace XMLReflection
         }
 
     }
-    
-
-    public class MySetting : XMLBase
-    {
-        //string
-
-        public string name = string.Empty;
-
-
-        //基元类型
-
-        public bool flag = true;
-
-        public byte age = 0;
-
-        public int id = 0;
-
-        public char character = '0';
-
-        public long size = 0L;
-
-        public float height = 0f;
-
-        public double width = 0d;
-
-        public decimal depth = 0m;
-
-
-        //结构和类
-
-        public Rectangle rect = new Rectangle();
-
-        public Point pos = new Point();
-
-        public DateTime date = DateTime.Now;
-
-
-    }
-
-}
-
-namespace Extention
-{
-
-
-    public static class Rectangle_Extention
-    {
-        public static Rectangle Parse(this Rectangle obj, string text)
-        {
-            text = text.Replace("}", "");
-
-            obj.X = int.Parse(text.Split(',')[0].Split('=')[1]);
-            obj.Y = int.Parse(text.Split(',')[1].Split('=')[1]);
-            obj.Width = int.Parse(text.Split(',')[2].Split('=')[1]);
-            obj.Height = int.Parse(text.Split(',')[3].Split('=')[1]);
-
-            return obj;
-        }
-    }
-
-    public static class RectangleF_Extention
-    {
-        public static RectangleF Parse(this RectangleF obj, string text)
-        {
-            text = text.Replace("}", "");
-
-            obj.X = float.Parse(text.Split(',')[0].Split('=')[1]);
-            obj.Y = float.Parse(text.Split(',')[1].Split('=')[1]);
-            obj.Width = float.Parse(text.Split(',')[2].Split('=')[1]);
-            obj.Height = float.Parse(text.Split(',')[3].Split('=')[1]);
-
-            return obj;
-        }
-    }
-
-    public static class Point_Extention
-    {
-        public static Point Parse(this Point obj, string text)
-        {
-            text = text.Replace("}", "");
-
-            obj.X = int.Parse(text.Split(',')[0].Split('=')[1]);
-            obj.Y = int.Parse(text.Split(',')[1].Split('=')[1]);
-
-            return obj;
-        }
-    }
-
-    public static class PointF_Extention
-    {
-        public static PointF Parse(this PointF obj, string text)
-        {
-            text = text.Replace("}", "");
-
-            obj.X = float.Parse(text.Split(',')[0].Split('=')[1]);
-            obj.Y = float.Parse(text.Split(',')[1].Split('=')[1]);
-
-            return obj;
-        }
-    }
-
-    public static class DateTime_Extention
-    {
-        public static DateTime Parse(this DateTime obj, string text)
-        {
-            return Convert.ToDateTime(text);
-        }
-    }
-
 
 }
